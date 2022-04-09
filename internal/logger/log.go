c21cc2990d3f1f6c57093f197e188f5553efdf99
Nicholas Kralles <nkralles@gmail.com>
2022-04-09 14:28:50 -0400

    test for dev null

--- internal/logger/log.go
+++ internal/logger/log.go
@@ -4,6 +4,7 @@ import (
 	"github.com/nkralles/masters-web/internal/cfg"
 	"gitlab.cloud.n-ask.com/n-ask/fancylog"
 	"os"
+	"syscall"
 )
 
 var Internal *fancylog.Logger
@@ -11,14 +12,15 @@ var REST *fancylog.Logger
 var PG *fancylog.Logger
 
 func init() {
+	devnull := os.NewFile(uintptr(syscall.Stdin), os.DevNull)
 	if cfg.Config.Verbose {
 		Internal = fancylog.NewWithName("internal", os.Stdout).WithColor().WithTimestamp().WithDebug()
 		Internal.Debug("Running in Verbose Debug level")
 		REST = fancylog.NewWithName("api", os.Stdout).WithColor().WithTimestamp().WithDebug()
-		PG = fancylog.NewWithName("postgres", os.Stdout).WithColor().WithTimestamp().WithDebug()
+		PG = fancylog.NewWithNameAndError("postgres", os.Stdout, os.Stdout).WithColor().WithTimestamp().WithDebug()
 	} else {
 		Internal = fancylog.NewWithName("internal", os.Stdout).WithColor().WithTimestamp()
 		REST = fancylog.NewWithName("api", os.Stdout).WithColor().WithTimestamp()
-		PG = fancylog.NewWithName("postgres", os.Stdout).WithColor().WithTimestamp()
+		PG = fancylog.NewWithNameAndError("postgres", devnull, os.Stdout).WithColor().WithTimestamp().WithDebug()
 	}
 }
